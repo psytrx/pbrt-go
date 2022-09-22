88b535424ab66b873f91f5430731e40b6ddff9cb
fdumont <fj.dumont@gmail.com>
2022-09-22 09:51:54 +0200

    Normalized log messages

--- cmd/render.go
+++ cmd/render.go
@@ -26,22 +26,22 @@ func start() {
 	film := pbrt.Render(options)
 	d := time.Since(t0)
 
-	log.Printf("render finished in %v", d)
+	log.Printf("finished render in %v", d)
 
 	if OUTPUT_FILENAME != "" {
 		writeFilm(film, OUTPUT_FILENAME)
-		log.Printf("written film to %s", OUTPUT_FILENAME)
+		log.Printf("written film to file '%s'", OUTPUT_FILENAME)
 	}
 }
 
 func writeFilm(film film.Film, filename string) {
-	img := film.ImageRGBA()
-
 	f, err := os.Create(OUTPUT_FILENAME)
 	if err != nil {
 		log.Fatalf("could not create output file '%s': %s", OUTPUT_FILENAME, err)
 	}
 	defer f.Close()
+
+	img := film.ImageRGBA()
 
 	if err := jpeg.Encode(f, img, &jpeg.Options{Quality: 100}); err != nil {
 		log.Fatalf("could not encode output image: %s", err)
